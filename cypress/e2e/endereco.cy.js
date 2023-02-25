57eebcb040e9a5e929b4dc6661c0092a276856fc
Renan Veras <renan.veras96@gmail.com>
2023-02-25 01:42:06 -0300

    Adicionando massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,8 @@
 /// <reference types="cypress" />
 
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
+
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
@@ -11,7 +14,24 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
     
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-            //cadastro de endereço
+        EnderecoPage.editarEnderecoFaturamento('João', 'Silva', 'Facebook', 'Brasil', 'Av Paulista', '1', 'São Paulo', 'São Paulo', '06121101', '11900000000', 'email@ebac.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 
+    it.only('Deve fazer cadastro de faturamento com sucesso usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
 });
